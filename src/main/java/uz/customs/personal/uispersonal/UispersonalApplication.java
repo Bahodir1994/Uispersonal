3d2855f6b5611bbb38ca77e1ae30c057cd5dae5b
Bahodir <imomuddinov@inbox.ru>
2023-12-13 15:42:03 +0500

    first commit

---
+++ src/main/java/uz/customs/personal/uispersonal/UispersonalApplication.java
@@ -0,0 +1,13 @@
+package uz.customs.personal.uispersonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UispersonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UispersonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/customs/personal/uispersonal/UispersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.customs.personal.uispersonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UispersonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
